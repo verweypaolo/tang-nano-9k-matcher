db633c0b37a40938e2098a639762ad1e3c3a43a1
PaoloV99 <140324664+PaoloV99@users.noreply.github.com>
2026-07-08 15:27:36 +0200

    Add CHECKSUM state

--- message_rx.v
+++ message_rx.v
@@ -35,10 +35,12 @@ reg [3:0] msgState; // track state
 reg [7:0] checksumAcc; // accumulate XOR of every received byte, check checksum at the end
 reg byteCounter; // track which byte in states that have two bytes
 reg [$clog2(TIMEOUT_CYCLES):0] byteWaitCounter;
+reg messageReady;
 
 // errors
-reg sentinelError; // assert if erroneous sentinel received
+reg sentinelError; // assert if sentinel doesn't match
 reg timeOutError; // assert if more than TIMEOUT_CYCLES cycles before receiving a byte
+reg checksumError; // assert if checksum doesn't match
 
 // for byteReady edge
 reg byteReadyPrev;
@@ -49,6 +51,7 @@ reg [7:0] msgType;
 reg [15:0] orderID;
 reg [7:0] side; // 1 if sell
 reg [15:0] price;
+reg [15:0] quantity;
 
 localparam MSG_STATE_IDLE = 0;
 localparam MSG_STATE_SENTINEL  = 1; // validate sentinel byte
@@ -64,13 +67,16 @@ initial begin
     checksumAcc = 0;
     byteCounter = 0;
     byteWaitCounter = 0;
+    messageReady = 0;
     sentinelError = 0;
     timeOutError = 0;
+    checksumError = 0;
     byteReadyPrev = 0;
     msgType = 0;
     orderID = 0;
     side = 0;
     price = 0;
+    quantity = 0;
 end
 
 always @(posedge clk) begin
@@ -84,6 +90,8 @@ always @(posedge clk) begin
                 checksumAcc <= 0;
                 sentinelError <= 0;
                 timeOutError <= 0;
+                checksumError <= 0;
+                messageReady <= 0;
                 msgState <= MSG_STATE_SENTINEL;
             end
         end
@@ -156,6 +164,41 @@ always @(posedge clk) begin
                 end else begin
                     byteCounter <= byteCounter + 1;
                 end
+            end else if (byteWaitCounter == TIMEOUT_CYCLES - 1) begin
+                timeOutError <= 1;
+                msgState <= MSG_STATE_IDLE;
+            end else begin
+                byteWaitCounter <= byteWaitCounter + 1;
+            end
+        end
+        MSG_STATE_QUANTITY: begin
+            if (byteReadyEdge) begin
+                quantity <= {quantity[7:0], dataIn};
+                checksumAcc <= checksumAcc ^ dataIn;
+                byteWaitCounter <= 0;
+
+                if (byteCounter == 1) begin
+                    byteCounter <= 0;
+                    msgState <= MSG_STATE_CHECKSUM;
+                end else begin
+                    byteCounter <= byteCounter + 1;
+                end
+            end else if (byteWaitCounter == TIMEOUT_CYCLES - 1) begin
+                timeOutError <= 1;
+                msgState <= MSG_STATE_IDLE;
+            end else begin
+                byteWaitCounter <= byteWaitCounter + 1;
+            end
+        end
+        MSG_STATE_CHECKSUM: begin
+            if (byteReadyEdge) begin
+                if (dataIn == checksumAcc) begin
+                    messageReady <= 1;
+                end else begin
+                    checksumError <= 1;
+                end
+                byteWaitCounter <= 0;
+                msgState <= MSG_STATE_IDLE;
             end else if (byteWaitCounter == TIMEOUT_CYCLES - 1) begin
                 timeOutError <= 1;
                 msgState <= MSG_STATE_IDLE;
