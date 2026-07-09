219dfb9fc0990ca3cf8e8b554bb9601e4e7a6890
PaoloV99 <140324664+PaoloV99@users.noreply.github.com>
2026-07-09 15:08:29 +0200

    Add helper tasks to testbench

--- message_rx_tb.v
+++ message_rx_tb.v
@@ -19,6 +19,7 @@ module message_rx_tb;
 
     localparam BAUD_DIVISOR = 8; // faster for simulation purposes
 
+
     message_rx
     #(
         .BAUD_DIVISOR(BAUD_DIVISOR) 
@@ -39,5 +40,39 @@ module message_rx_tb;
     // clock generation
     initial clk = 0;
     always #5 clk = ~clk;
+
+    initial begin
+        uart_rx_line = 1; // initialize to high as expected for UART
+    end
+
+    // To help with proper UART timing
+    task hold_for_bit_period;
+        integer i;
+        begin
+            for (i = 0; i < BAUD_DIVISOR; i = i + 1) begin
+                @(posedge clk);
+            end
+        end
+    endtask
+
+    task send_byte;
+        input [7:0] data;
+        integer i;
+        begin
+            uart_rx_line = 0; // start bit
+            hold_for_bit_period; // wait one bit frame
+            
+            for (i = 0; i < 8; i = i + 1) begin
+                uart_rx_line = data[i];
+                hold_for_bit_period;
+            end
+            
+            uart_rx_line = ^data ^ 0; // parity bit (^data = 0 if even number of ones), hardcoded for even parity
+            hold_for_bit_period;
+            
+            uart_rx_line = 1; // stop bit
+            hold_for_bit_period;
+        end
+    endtask
 
 endmodule
