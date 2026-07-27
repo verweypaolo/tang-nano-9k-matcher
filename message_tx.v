84a227c56bdd15ca1198b7e34b356ce490268259
PaoloV99 <140324664+PaoloV99@users.noreply.github.com>
2026-07-27 15:13:39 +0200

    Add MTX_IDLE state

--- message_tx.v
+++ message_tx.v
@@ -23,4 +23,75 @@ module message_tx
     output reg sendWhileBusyError
 );
 
+// store local copy of values to work with on sendMessageValid trigger
+reg [7:0] msgType;
+reg [15:0] orderID;
+reg [7:0] outcome;
+reg [15:0] price;
+reg [15:0] quantity;
+
+wire [7:0] checksum = SENTINEL
+                     ^ msgType
+                     ^ orderID[15:8]
+                     ^ orderID[7:0]
+                     ^ outcome
+                     ^ price[15:8]
+                     ^ price[7:0]
+                     ^ quantity[15:8]
+                     ^ quantity[7:0];
+
+reg [3:0] mtxState;
+
+reg sendMessageValidPrev;
+wire sendMessageValidEdge = sendMessageValid & !sendMessageValidPrev;
+
+localparam MTX_STATE_IDLE = 0;
+localparam MTX_STATE_SENTINEL = 1;
+
+
+initial begin
+    msgType = 0;
+    orderID = 0;
+    outcome = 0;
+    price = 0;
+    quantity = 0;
+
+    mtxState = 0;
+    sendMessageValidPrev = 0;
+
+    messageSent = 0;
+    txBusy = 0;
+    sendWhileBusyError = 0;
+end
+
+always @(posedge clk) begin
+    sendMessageValidPrev <= sendMessageValid;
+end
+
+always @(posedge clk) begin
+    if (sendMessageValidEdge && mtxState != MTX_STATE_IDLE) begin
+        sendWhileBusyError <= 1;
+    end
+
+    case (mtxState)
+        MTX_STATE_IDLE: begin
+            messageSent <= 0;
+            sendWhileBusyError <= 0;
+            if (sendMessageValidEdge) begin
+                // latch current message value
+                msgType <= msgTypeIn;
+                orderID <= orderIDIn;
+                outcome <= outcomeIn;
+                price <= priceIn;
+                quantity <= quantityIn;
+                // indicate transmission has started/in progress
+                txBusy <= 1;
+                // move state
+                mtxState <= MTX_STATE_SENTINEL;
+
+            end
+        end
+    endcase
+end
+
 endmodule
